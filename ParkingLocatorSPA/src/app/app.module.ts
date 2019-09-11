1b8f4ff97e52e853163e8655cb9b4742e86c11d5
Quinn Meagher <qmeagher@ostusa.com>
2019-09-11 11:50:06 -0400

    Added weather widget and tested some layout features.

--- ParkingLocatorSPA/src/app/app.module.ts
+++ ParkingLocatorSPA/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbCardModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbCardModule, NbIconModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -25,6 +25,7 @@ import { MainMapComponent } from './main-map/main-map.component';
     AppRoutingModule,
     HttpClientModule,
     NbCardModule,
+    NbIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
