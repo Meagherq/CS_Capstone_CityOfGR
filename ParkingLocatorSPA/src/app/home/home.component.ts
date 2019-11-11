e860fd535362e9ed23df9760ef8079adf3a26c41
Meagherq <34166781+Meagherq@users.noreply.github.com>
2019-11-10 22:28:28 -0500

    Extended Geocoder Size

--- ParkingLocatorSPA/src/app/home/home.component.ts
+++ ParkingLocatorSPA/src/app/home/home.component.ts
@@ -9,92 +9,6 @@ import { Feature } from 'geojson';
 
 
 declare let L: any;
-// let parkingSpots: any;
-// let Layer: mapboxgl.Layer;
-// const parkingspot =
-// [[
-//     42.966133652813,
-//     -85.670216067712
-// ],
-// [
-//     42.966133652813,
-//     -85.670215097108
-// ],
-// [
-//     42.966078458585,
-//     -85.670248719741
-// ],
-// [
-//     42.966078458585,
-//     -85.670215097108
-// ]];
-
-// const parkingspot2 = [[
-//     42.961997937653,
-//     -85.672433426147
-//     ],
-//     [
-//     42.961997414251,
-//     -85.672467042291
-//     ],
-//     [
-//     42.962052280431,
-//     -85.672468625172
-//     ],
-//     [
-//     42.962052803832,
-//     -85.672435008999
-//     ],
-//     [
-//     42.961997937653,
-//     -85.672433426147
-//     ]];
-
-// const parkingspot3 = [[
-//             42.961770755288,
-//             -85.666767464271
-//         ],
-//         [
-//         42.961770103943,
-//         -85.666801076388
-//         ],
-//         [
-//         42.96182496394,
-//         -85.666803047612
-//         ],
-//         [
-//         42.961825615286,
-//         -85.666769435465
-//         ],
-//         [
-//          42.961770755288,
-//         -85.666767464271
-//         ]];
-// const parkingspot4 = [[
-//             42.9756814769,
-//             -85.672714419956
-
-//             ],
-//             [
-//              42.975656961224,
-//             -85.672718469417
-
-//             ],
-//             [
-//             42.975663569971,
-//             -85.672792662621
-
-//             ],
-//             [
-//              42.975688085663,
-//             -85.672788611964
-
-//             ],
-//             [
-//             42.9756814769,
-//             -85.672714419956
-
-//             ]];
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -111,7 +25,7 @@ export class HomeComponent implements OnInit {
 
     httpOptions = {
         headers: new HttpHeaders({
-          'Content-Type':  'application/json',
+            'Content-Type':  'application/json',
         }),
     };
     availableSpots: any = 0;
@@ -185,8 +99,6 @@ export class HomeComponent implements OnInit {
 
     mapParkingSpots() {
         this.parkingService.getSocrataMasterList().subscribe(data => {
-
-
             const socrataMaster: any = data;
             console.log(socrataMaster);
             socrataMaster.forEach(element => {
@@ -196,7 +108,6 @@ export class HomeComponent implements OnInit {
                     geometry: {
                         type: 'Polygon',
                         coordinates: [
-                            // element.boundingBox[0][0], element.boundingBox[0][1], element.boundingBox[2][0], element.boundingBox[2][1]
                             [
                                 [
                                     element.boundingBox[0][0], element.boundingBox[0][1]
@@ -238,45 +149,4 @@ export class HomeComponent implements OnInit {
             });
           });
     }
-
-
-    focusOutFunction() {
-        this.searchResult = '';
-      }
-    // Old function still here for no reason
-    //   onKeydown(event: any) {
-    //     if (event.key === 'Enter') {
-    //       console.log(event);
-    //       console.log(this.searchResult);
-    //       const baseSearch = this.searchResult;
-    //       this.searchResult = this.searchResult + ` Grand Rapids Michigan`;
-    //       this.searchOSM().subscribe(x => {
-    //         this.searchValue = x;
-
-    //         if (baseSearch === 'mySpot') {
-    //           const polygon = L.polygon(parkingspot);
-    //           const polygon2 = L.polygon(parkingspot2);
-    //           const polygon3 = L.polygon(parkingspot3);
-    //           const polygon4 = L.polygon(parkingspot4);
-    //           polygon.addTo(this.map);
-    //           polygon2.addTo(this.map);
-    //           polygon3.addTo(this.map);
-    //           polygon4.addTo(this.map);
-    //           polygon.setStyle({fillColor: '#dddddd'});
-    //           this.map.flyTo(new L.LatLng(    42.961997414251,
-    //               -85.672467042291
-    //               ), 18);
-    //         } else {
-    //           console.log(this.searchValue);
-    //           console.log(this.searchValue[0].boundingbox[0] + ` - ` + this.searchValue[0].boundingbox[2]);
-    //           //this.searchSpots();
-    //           //this.showParkingSpots(parkingSpots);
-    //           this.map.flyTo(new L.LatLng(this.searchValue[0].boundingbox[0], this.searchValue[0].boundingbox[2]), 18);
-    //         }
-    //       });
-    //       this.searchResult = '';
-    //       const showMap = document.getElementById('map');
-    //       showMap.scrollIntoView();
-    //     }
-    //   }
 }
