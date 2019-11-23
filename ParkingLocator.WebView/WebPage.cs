90bee3911d2a94af2440b486bb74c429fce3b2a6
Meagherq <34166781+Meagherq@users.noreply.github.com>
2019-11-23 13:44:14 -0500

    Added WebView project

---
+++ ParkingLocator.WebView/WebPage.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace WorkingWithWebview
+{
+    public class WebPage : ContentPage
+    {
+        public WebPage()
+        {
+            var browser = new WebView();
+            browser.Source = "http://2da9636c.ngrok.io";
+            Content = browser;            
+        }
+    }
+}
+
