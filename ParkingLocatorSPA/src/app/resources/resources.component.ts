d4f3a2bb0e545aaa22b11b2cf311989b9e5ec5c2
fountaig <fountaig@mail.gvsu.edu>
2019-12-04 18:31:29 -0500

    more stuff

--- ParkingLocatorSPA/src/app/resources/resources.component.ts
+++ ParkingLocatorSPA/src/app/resources/resources.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-resources',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resources.component.scss']
 })
 export class ResourcesComponent implements OnInit {
+  
+  public isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(map(result => result.matches));
 
-  constructor() { }
+  constructor( private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit() {
   }
 
+
+
+  goDash() {
+    window.location.href='https://www.grandrapidsmi.gov/Residents/Parking-and-Mobility/DASH-the-Downtown-Area-Shuttle';
+  }
+  goCity() {
+    window.location.href='https://www.grandrapidsmi.gov';
+  }
 }
